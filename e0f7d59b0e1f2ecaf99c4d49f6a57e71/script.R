425d49b14eb2ffec99d77261eff94e7c89d54bef
pbiecek <przemyslaw.biecek@gmail.com>
2013-08-27 15:23:31 +0200

    now it should work

--- e0f7d59b0e1f2ecaf99c4d49f6a57e71/script.R
+++ e0f7d59b0e1f2ecaf99c4d49f6a57e71/script.R
@@ -2,5 +2,9 @@
 # load plot from github
 library(ggplot2)
 library(RCurl)
-plotOb <- getURL("https://github.com/pbiecek/graphGallery/raw/master/e0f7d59b0e1f2ecaf99c4d49f6a57e71/plot.rda")
-print(plotOb)
+plotOb <- getBinaryURL("https://github.com/pbiecek/graphGallery/raw/master/e0f7d59b0e1f2ecaf99c4d49f6a57e71/plot.rda")
+tf <- tempfile()
+writeBin(plotOb, tf)
+name <- load(tf)
+unlink(tf)
+get(name)
