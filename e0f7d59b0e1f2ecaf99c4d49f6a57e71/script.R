d5b5e47c05207c685fad2b734540ebb26427b3ea
pbiecek <przemyslaw.biecek@gmail.com>
2013-08-27 15:25:48 +0200

    loader update

--- e0f7d59b0e1f2ecaf99c4d49f6a57e71/script.R
+++ e0f7d59b0e1f2ecaf99c4d49f6a57e71/script.R
@@ -2,7 +2,7 @@
 # load plot from github
 library(ggplot2)
 library(RCurl)
-plotOb <- getBinaryURL("https://github.com/pbiecek/graphGallery/raw/master/e0f7d59b0e1f2ecaf99c4d49f6a57e71/plot.rda")
+plotOb <- getBinaryURL("https://raw.github.com/pbiecek/graphGallery/master/e0f7d59b0e1f2ecaf99c4d49f6a57e71/plot.rda")
 tf <- tempfile()
 writeBin(plotOb, tf)
 name <- load(tf)
