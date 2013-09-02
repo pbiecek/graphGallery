d0575e9d64b49358ee3a69490ba72d93e109ed32
pbiecek <przemyslaw.biecek@gmail.com>
2013-09-02 12:28:16 +0200

    now it uses rawgithub instead of raw.github

--- 57428a122d587b33a054c2a245076518/script.R
+++ 57428a122d587b33a054c2a245076518/script.R
@@ -2,7 +2,7 @@
 # load plot from github
 library(ggplot2)
 library(RCurl)
-plotOb <- getBinaryURL("https://raw.github.com/pbiecek/graphGallery/master/57428a122d587b33a054c2a245076518/plot.rda")
+plotOb <- getBinaryURL("https://rawgithub.com/pbiecek/graphGallery/master/57428a122d587b33a054c2a245076518/plot.rda")
 tf <- tempfile()
 writeBin(plotOb, tf)
 name <- load(tf)
