6573a3ed103a91dc2416b8ce18d02acbc289729c
pbiecek <przemyslaw.biecek@gmail.com>
2013-10-03 11:17:47 +0200

    cleanArchive functions is working now

--- 071c8743a176a06309b88628eabe1826/script.R
+++
@@ -1,6 +0,0 @@
-
-# load plot from github
-library(ggplot2)
-library(RCurl)
-plotOb <- getURL("https://github.com/pbiecek/graphGallery/raw/master/071c8743a176a06309b88628eabe1826/plot.rda")
-print(plotOb)
--- 7f0b32dc9c666e3f6217691d459a5e55/script.R
+++
@@ -1,6 +0,0 @@
-
-# load plot from github
-library(ggplot2)
-library(RCurl)
-plotOb <- getURL("https://github.com/pbiecek/graphGallery/raw/master/7f0b32dc9c666e3f6217691d459a5e55/plot.rda")
-print(plotOb)
