756cbc0cd3fb8114c90215f902591aaa13c43882
pbiecek <przemyslaw.biecek@gmail.com>
2013-10-11 18:32:34 +0200

    update

--- tags.js
+++ tags.js
@@ -1,2 +1,2 @@
 var availableTags = [
-"country", "dtv", "f011", "f053", "f192"];
+"2003", "2012", "area", "baseline", "ccr", "change", "competency.clusters", "computer", "country", "credit", "data", "dtv", "excelfile", "experiment", "f011", "f053", "f192", "feed", "gg", "ggplot", "group", "in", "item", "math", "paper", "performance", "performance", "pisa", "pisa2003", "pisa2012", "problem", "reading", "science", "solving", "spanish", "title"];
